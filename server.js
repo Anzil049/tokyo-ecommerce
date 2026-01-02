dd6258d0d88c69ab11988750407ccd0e8c40fcbb
anzil049 <anzil049@gmail.com>
2026-01-02 10:47:29 +0530

    redirection fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const express=require("express");
-const mongoose=require("mongoose");
-const path=require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 require('./config/passport'); // just load strategy
@@ -10,54 +10,54 @@ const dns = require('node:dns');
 
 dns.setDefaultResultOrder('ipv4first');
 
-const app=express();
+const app = express();
 
-const productRoutes=require("./routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
-const teamRoutes = require("./routes/teamRoutes");  
+const teamRoutes = require("./routes/teamRoutes");
 const authRoutes = require('./routes/authRoutes');
-const adminRoutes = require('./routes/adminRoutes'); 
+const adminRoutes = require('./routes/adminRoutes');
 const cartRoutes = require('./routes/cartRoutes');
-const wishlistRoutes=require('./routes/wishlistRoutes');
-const addressRoutes=require('./routes/addressRoutes');
-const paymentRoutes=require('./routes/paymentRoutes');
+const wishlistRoutes = require('./routes/wishlistRoutes');
+const addressRoutes = require('./routes/addressRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 const couponRoutes = require('./routes/couponRoutes');
-const orderRoutes=require('./routes/orderRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const statsRoutes = require('./routes/statsRoutes');
-const saleRoutes=require('./routes/saleRoutes');
+const saleRoutes = require('./routes/saleRoutes');
 const startScheduler = require('./jobs/saleScheduler'); // Import
 
 
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //-------------------------Middlewares---------------------------
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(passport.initialize());
 
 //---------------------------Database Connection--------------------------
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
+    .then(() => {
         console.log("MongoDB Connected Successfully ✅")
     })
-    .catch((err)=>{
-        console.log("MongoDB Connection Failed ❌",err);
+    .catch((err) => {
+        console.log("MongoDB Connection Failed ❌", err);
     })
 
 //--------------------------API Routes-----------------------------  
 
 // Import the Product Routes (the Controller logic is now handled here)
 
-app.use("/api/products",productRoutes);     // All requests to /api/products go to productRoutes.js
-app.use("/api/categories", categoryRoutes); 
-app.use("/api/teams", teamRoutes);    
+app.use("/api/products", productRoutes);     // All requests to /api/products go to productRoutes.js
+app.use("/api/categories", categoryRoutes);
+app.use("/api/teams", teamRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/cart', cartRoutes);
@@ -69,7 +69,7 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/stats', statsRoutes);
-app.use('/api/sales',saleRoutes);
+app.use('/api/sales', saleRoutes);
 
 
 
@@ -79,16 +79,20 @@ app.use('/api/sales',saleRoutes);
 
 //---User Pages---
 
-app.get("/User",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","User","index.html"))
+app.get('/', (req, res) => {
+    res.redirect('/user');
 });
 
-app.get("User/products",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","User","products.html"))
+app.get("/user", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "User", "index.html"))
+});
+
+app.get("user/products", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "User", "products.html"))
 })
 
-app.get("User/cart",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public","User","cart.html"))
+app.get("user/cart", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "User", "cart.html"))
 })
 
 //---Admin pages---
@@ -109,9 +113,9 @@ startScheduler();
 
 //-------------------------Start Server-------------------------------
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(
-`server running at http://localhost:${PORT}
+        `server running at http://localhost:${PORT}
 User side running at http://localhost:${PORT}/User
-Admin side running at http://localhost:${PORT}/Admin`); 
+Admin side running at http://localhost:${PORT}/Admin`);
 });         
